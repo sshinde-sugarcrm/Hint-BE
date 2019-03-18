83bbaec28df124c1df2a0738133d36a6459e7cbf
SAISH-SHINDE <35019888+SAISH-SHINDE@users.noreply.github.com>
2019-03-17 18:17:49 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var index = require('./routes/index');
+var stage2 = require('./routes/stage2');
+var logout = require('./routes/logout');
+var app = express();
+
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+
+app.use(cors(corsOptions));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+app.use('/stage2', stage2);
+app.listen(3001,console.log('React App Listening on port 3001!'));
+
+app.post('/logout',logout.logout);
+module.exports = app;
---
+++ routes/stage2.js
@@ -0,0 +1,72 @@
+let express = require('express');
+let router = express.Router();
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host     : "hint.c6uw0k5bgh7m.us-west-1.rds.amazonaws.com",
+    user     : "root",
+    password : "hintdemo",
+    database:  "hint",
+    port     : 3306,
+    timeout: 60000
+});
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Database connection failed 1: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database.');
+});
+
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+var dummy="dummy";
+
+
+router.post('/token', function (req, res, next) {
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log("reached login");
+    let array = [];
+    array.push([email,password]);
+
+    connection.query('SELECT * FROM user WHERE email = ?', [email], function (err, results) {
+        if (err) throw err;
+        console.log('--> ', results);
+
+        if (results === null || results.length === 0) {
+            console.log("bad request");
+            return res.json({status: 401});
+        }
+        else if (results[0]) {
+            console.log("results", results);
+            let pass = results[0].password;
+            let email=results[0].email;
+            if (pass === password) {
+                console.log("success", results[0]);
+                return res.json({message: "Logged in Successfully", email: email, status: 200, tutor:false});
+            }
+            else {
+                return res.json({status: 401});
+            }
+        }
+    });
+});
+
+router.post('/logout', function(req,res){
+    var session=req.session;
+    dummy="dummy";
+    console.log("In logout ", req.session.user)
+    session.user = null;
+    session.destroy();
+    res.json({
+        status:'200',
+        message : "Logged Out."
+    });
+});
+
+module.exports = router;
